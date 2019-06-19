efaddcc66f2357bb23198bc467e11c75e5e083b9
Mitchell <mmoscovics@yahoo.com>
2019-06-19 17:30:07 +0000

    updated variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {printf("%s%d%s", "Size of a char: ", sizeof(char), " byte(s)\n");
 printf("%s%d%s", "Size of a int: ", sizeof(int), " byte(s)\n");
-printf("%s%d%s", "Size of a long int: ", sizeof(long int), " byte(s)\n");
+printf("%s%d%s", "Size of a long int: ", sizeof(long), " byte(s)\n");
 printf("%s%d%s", "Size of a long long: ", sizeof(long long), " byte(s)\n");
 printf("%s%d%s", "Size of a float: ", sizeof(float), " byte(s)\n");
 return (0);
