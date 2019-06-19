af756362c66f9667e1e589436ad1f7c608cf6e6c
Mitchell <mmoscovics@yahoo.com>
2019-06-19 17:53:07 +0000

    updated text

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{printf("%s%d%s", "Size of a char: ", sizeof(char), " byte(s)\n");
-printf("%s%d%s", "Size of a int: ", sizeof(int), " byte(s)\n");
+{
+printf("%s%d%s", "Size of a char: ", sizeof(char), " byte(s)\n");
+printf("%s%d%s", "Size of an int: ", sizeof(int), " byte(s)\n");
 printf("%s%d%s", "Size of a long int: ", sizeof(long), " byte(s)\n");
 printf("%s%d%s", "Size of a long long int: ", sizeof(long long), " byte(s)\n");
 printf("%s%d%s", "Size of a float: ", sizeof(float), " byte(s)\n");
