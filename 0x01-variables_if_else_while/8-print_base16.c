ae48e059dccb4606ada2cb78600b625f37aa7efe
Mitchell <mmoscovics@yahoo.com>
2019-06-20 19:50:31 +0000

    create task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num, ch;
+for (num = '0' ; num <= '9' ; num++)
+{
+	putchar(num);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
