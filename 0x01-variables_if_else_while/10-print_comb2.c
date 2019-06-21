ad5801072a3124954106241bb61d4bbf620e70f9
Mitchell <mmoscovics@yahoo.com>
2019-06-21 01:14:55 +0000

    create task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0 ; num < 100 ; num++)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
