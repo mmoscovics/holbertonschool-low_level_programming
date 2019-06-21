4ee6cc8410aaa944cfd02406956a9e7732d926b3
Mitchell <mmoscovics@yahoo.com>
2019-06-21 01:18:11 +0000

    updated script to end without a coma

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -13,8 +13,12 @@ for (num = 0 ; num < 100 ; num++)
 {
 putchar((num / 10) + '0');
 putchar((num % 10) + '0');
+while (num < 99)
+{
 putchar(',');
 putchar(' ');
+break;
+}
 }
 putchar('\n');
 return (0);
