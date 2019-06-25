8dfc3cff760abed413aaaddf0ac8c06297cd6242
Mitchell <mmoscovics@yahoo.com>
2019-06-25 21:43:41 +0000

    create task 7 and updated header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *@l: integers last digit
+ * Return: value.
+ * From Anthony: WHAT DOES PUTCHAR DO AND WHAT ARE YOU PASSING IT?
+ */
+int print_last_digit(int l)
+{
+	if (l >= 0)
+	{
+		l = l % 10;
+		_putchar('0' + l);
+		return (l);
+	}
+	else
+	{
+		l = l * -1;
+		l = l % 10;
+		_putchar('0' + l);
+		return (l);
+	}
+}
