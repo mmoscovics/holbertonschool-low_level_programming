0b964b2a7e000d9024af5eb58bf7c36fb1db9b78
Mitchell <mmoscovics@yahoo.com>
2019-06-26 17:21:24 +0000

    create task 10 and updated header

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int mult;
+	int num;
+	int val;
+	for (mult = 0 ; mult <= 9 ; mult++)
+	{
+		for (num = 0 ; num <= 9 ; num++)
+		{
+			val = mult * num;
+			if (num == 0)
+			{
+				_putchar(val + '0');
+			}
+			else if (num > 0 && val < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(val + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((val / 10) +'0');
+				_putchar((val % 10) +'0');
+			}
+		}
+		_putchar('\n');
+	}
+}
