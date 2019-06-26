84f198de0faab4da3ac0a9be826e5c0fe4335718
Mitchell <mmoscovics@yahoo.com>
2019-06-26 02:23:34 +0000

    create task 10 and update header

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *@num_one: first num
+ *@num_two: second num
+ * Return: add.
+ */
+int add(int num_one, int num_two)
+{
+	return (num_one + num_two);
+}
