c8173b570dc1e82fa64cb4fb1f0047939865a799
Mitchell <mmoscovics@yahoo.com>
2019-06-27 21:40:29 +0000

    create file for task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*
+*
+*
+*/
+int main(void)
+{
+	int num;
+	{
+		for (num = 1 ; num <= 100 ; num++)
+		{
+			if (num % 3 == 0 && num % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (num % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else if (num % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+				printf("%d ", num);
+			}
+		}
+		printf("\n");
+	}
+	return (0);
+}
