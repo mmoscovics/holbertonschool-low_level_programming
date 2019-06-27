a951f1f060803f169e0cbd7a528a47a9cdb9d381
Mitchell <mmoscovics@yahoo.com>
2019-06-27 21:15:09 +0000

    updated files for task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 
 /**
-* print_diagonal - tested function
+* print_square - tested function
 *@size: loop
 * Return: 0
 */
 void print_square(int size)
 {
-	int loop, space;
-	for (loop = 0 ; loop < size ; loop++)
+int loop, space;
+for (loop = 0 ; loop < size ; loop++)
+{
+	for (space = 0 ; space < size ; space++)
 	{
-		for (space = 0 ; space < size ; space++)
+		if (size > 0)
 		{
-			if (size > 0)
-			{
-				_putchar('#');
-			}
-			else
-			{
-				_putchar('\n');
-			}
+			_putchar('#');
 		}
-		_putchar('\n');
+		else
+		{
+			_putchar('\n');
+		}
 	}
+	_putchar('\n');
+}
 }
