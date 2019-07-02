81b51fe08c89bbc3df75afae08070cfe28f255cd
Mitchell <mmoscovics@yahoo.com>
2019-07-02 13:13:41 +0000

    create files for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _strlen - tested function
+*@s: length
+* Return: 0
+*/
+int _strlen(char *s)
+{
+int length;
+for (length = 0 ; s[length] != '\0' ; ++length)
+{
+}
+return (length);
+}
