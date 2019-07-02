d062f9355cc5115e4f347ce7a1ff6765381e8ea3
Mitchell <mmoscovics@yahoo.com>
2019-07-02 23:16:38 +0000

    create files for task 9 and updated header

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - tested function
+ *@dest: destination string
+ *@src: sorce string
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length, temp;
+
+	for (length = 0 ; src[length] <= '\0' ; ++length)
+		;
+	for (temp = 0 ; temp <= src[length] ; ++temp)
+	{
+		dest[temp] = src[temp];
+	}
+	return (dest);
+}
