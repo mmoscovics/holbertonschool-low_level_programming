8bcf888f3b63d72f0d32e0e222a583bf06c270bd
Mitchell <mmoscovics@yahoo.com>
2019-07-05 14:27:49 +0000

    Created directory with basic required files Created header file Created _putchar function Created files for task 0 Created README.md for directory

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcat - tested function
+ *@dest: destination variable
+ *@src: source variable
+ * Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len_one, len_two;
+
+	for (len_one = 0 ; dest[len_one] != '\0' ; len_one++)
+		;
+	for (len_two = 0 ; src[len_two] != '\0' ; len_two++, len_one++)
+		dest[len_one] = src[len_two];
+	dest[len_one] = '\0';
+	return (dest);
+}
