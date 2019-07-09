fe09114c9710b251fc6f9f6cabfc895ea72df5b9
Mitchell <mmoscovics@yahoo.com>
2019-07-09 19:51:59 +0000

    update task 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-	char *pos = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *s != '\0'; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			pos = &s[i];
-			return (pos);
+			return (s);
 		}
 	}
 	if (c == '\0')
-		return (pos);
+		return (s);
 	return (0);
 }
