4753cc4a46d0ae4c88c051b81aa0c6e410ffe364
Mitchell <mmoscovics@yahoo.com>
2019-07-09 15:12:29 +0000

    Create files for task 2 create 2.c file updated header file with function for task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+	return (s + i);
+}
