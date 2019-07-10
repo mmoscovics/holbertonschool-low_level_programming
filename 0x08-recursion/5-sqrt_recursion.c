67eab86a22ae20601a1450059bf76e14d101b37f
Mitchell <mmoscovics@yahoo.com>
2019-07-10 19:14:41 +0000

    created files for task 5 create 5.c file updated header file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+int _sqhelp(int count, int n);
+
+/**
+ * _sqrt_recursion - tested function
+ *@n: tested number
+ * Return: square root of tested number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (_sqhelp(n - 1, n));
+}
+/**
+ * _sqhelp - helper function
+ *@count: counter
+ *@n: original tested number
+ * Return: counter
+ */
+int _sqhelp(int count, int n)
+{
+	if (count == 1)
+		return (-1);
+	if (count * count == n)
+		return (count);
+	return (_sqhelp(count - 1, n));
+}
