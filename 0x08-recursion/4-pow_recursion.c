e8ecf0c4d028615c9a717bbeffc118a8378310a4
Mitchell <mmoscovics@yahoo.com>
2019-07-10 18:17:58 +0000

    created files for task 4 create 4.c file updated header file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - tested function
+ *@x: base value
+ *@y: raised to power
+ * Return: powered value
+ */
+int _pow_recursion(int x , int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (x == 0)
+		return (0);
+	return (x * _pow_recursion(x, y - 1));
+}
