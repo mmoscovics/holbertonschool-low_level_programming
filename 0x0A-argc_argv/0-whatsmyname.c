ec417f81c3f6c85536fb1a9e55e6b1762338149a
Mitchell <mmoscovics@yahoo.com>
2019-07-15 20:18:39 +0000

    created task 0 file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - tested function
+ *@argc: argument count
+ *@argv: argument vector
+ * Return: 0
+ */
+
+int main(void argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
