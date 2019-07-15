f8c51b3970c0eb6ef3d0e679015fe2bd5b8b3b4a
Mitchell <mmoscovics@yahoo.com>
2019-07-15 20:39:12 +0000

    create task 1 file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - tested function
+ *@argc: argument counter
+ *@argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
