b5f244a66e7783c8905f8da2648f74d70c25d357
Mitchell <mmoscovics@yahoo.com>
2019-07-16 20:15:46 +0000

    update task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int length1, length2, count;
 	char *cat;
 
+	if (s1 == NULL)
+		s1 = 0;
+	if (s2 == NULL)
+		s2 = 0;
 	for (length1 = 0; s1[length1] != '\0'; length1++)
 		;
 	for (length2 = 0; s2[length2] != '\0'; length2++)
