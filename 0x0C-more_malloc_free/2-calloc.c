8566101f29d8076b1e1ff0c980da4dc91215e144
Mitchell <mmoscovics@yahoo.com>
2019-07-18 18:56:38 +0000

    create files for task 2 create 2.c file updated header file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - tested function, allocates memory for an array
+ *@nmemb: memory size in n
+ *@size: size of array
+ * Return: NULL if fail or nmemb or size is 0, or returns a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int count, length;
+	char *point1, *point2;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	length = nmemb * size;
+
+	if ((point1 = point2 = malloc(length)) != NULL)
+	    for (count = 0; count < length; count++)
+		    *point1++ = 0;
+	return (point2);
+}
