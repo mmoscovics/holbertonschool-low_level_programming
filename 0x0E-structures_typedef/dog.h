e8bc7e3bc626f47ee5bea794270a88c1e1e1f460
Mitchell <mmoscovics@yahoo.com>
2019-07-22 17:34:47 +0000

    updated file for task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure containing variables
+ *@name: name of dog
+ *@owner: name of owner
+ *@age: age of dog
+ */
 struct dog
 {
 	char *name;
