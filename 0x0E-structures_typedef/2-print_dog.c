ee1e51cbb8812923b19d5b81814c707529cf7640
Mitchell <mmoscovics@yahoo.com>
2019-07-23 18:54:49 +0000

    update task 2 file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		if (d->owner ==NULL)
+		if (d->owner == NULL)
 			printf("Owner: %p\n", d->owner);
 		else
 			printf("Owner: %s\n", d->owner);
