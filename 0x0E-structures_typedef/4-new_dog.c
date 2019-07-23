dd9019575abdb4939c1107a2c6a2a6244870ba9c
Mitchell <mmoscovics@yahoo.com>
2019-07-23 18:33:54 +0000

    update task 4 file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,36 +15,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name1, *owner1;
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog)
+	for (count = 0; name[count] != '\0'; count++, length_name++)
+		;
+	length_name++;
+	name1 = malloc(sizeof(char) * length_name);
+	if (name1 == NULL)
 	{
-		for (count = 0; name[count] != '\0'; count++, length_name++)
-			;
-		length_name++;
-		name1 = malloc(sizeof(char) * length_name);
-		if (name1 == NULL)
-		{
-			free(dog);
-			return (NULL);
-		}
-		for (count = 0; count < length_name; count++)
-			name1[count] = name[count];
-		for (count = 0; owner[count] != '\0'; count++, length_owner++)
-			;
-		length_owner++;
-		owner1 = malloc(sizeof(char) * length_owner);
-		if (owner1 == NULL)
-		{
-			free(name1);
-			free(dog);
-			return (NULL);
-		}
-		for (count = 0; count < length_owner; count++)
-			owner1[count] = owner[count];
-		dog->name = name1;
-		dog->age = age;
-		dog->owner = owner1;
-		return (dog);
+		return (NULL);
 	}
-	return (NULL);
+	for (count = 0; owner[count] != '\0'; count++, length_owner++)
+		;
+	length_owner++;
+	owner1 = malloc(sizeof(char) * length_owner);
+	if (owner1 == NULL)
+	{
+		free(name1);
+		return (NULL);
+	}
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(name1);
+		free(owner1);
+		return (NULL);
+	}
+	for (count = 0; count < length_name; count++)
+		name1[count] = name[count];
+	for (count = 0; count < length_owner; count++)
+		owner1[count] = owner[count];
+	dog->name = name1;
+	dog->age = age;
+	dog->owner = owner1;
+	return (dog);
 }
