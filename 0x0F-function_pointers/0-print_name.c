cbfa1a9f436b568141e8276aa8a5b2b29212aa12
Mitchell <mmoscovics@yahoo.com>
2019-07-24 18:13:38 +0000

    create file for task 0 create 0.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - tested function, function to print name
+ *@name: name input
+ *@f: function pointer, points to name input
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
