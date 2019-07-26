1aadc9d13e5736098d37225243e89f3dcaef20fb
Mitchell <mmoscovics@yahoo.com>
2019-07-26 17:44:14 +0000

    create files for task 0 create 0.c file updated header file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list argpassed;
+	unsigned int iter;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start (argpassed, n);
+	for (iter = 0; iter < n; iter++)
+		sum += va_arg (argpassed, int);
+	va_end (argpassed);
+	return (sum);
+}
