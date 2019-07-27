0de7e300379114d817f2338e4433da315749ba2f
Mitchell <mmoscovics@yahoo.com>
2019-07-27 17:40:45 +0000

    create files for task 2 create 2.c file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - tested function, prints a string passed
+ *@separator: separator string
+ *@n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list argpass;
+	unsigned int iter;
+        char *place;
+
+	va_start(argpass, n);
+	for (iter = 0; iter < n; iter++)
+	{
+		place = va_arg(argpass, char *);
+		if (place == NULL)
+			printf("%p", place);
+		printf("%s", place);
+		va_end(argpass);
+		if (iter < n - 1)
+		{
+			if (separator == NULL)
+				break;
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
