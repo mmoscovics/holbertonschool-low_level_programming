4f49194eff8bcb3969a167ab1a6a90b9329487c6
Mitchell <mmoscovics@yahoo.com>
2019-07-27 19:59:39 +0000

    create files for task 3 create 3.c file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - tested function, print all format specifiers
+ *@format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int iter = 0;
+	char *word;
+
+	va_start(args, format);
+	while (format[iter] != '\0')
+	{
+		if (iter > 0 && (format[iter] == 'c'
+		    || format[iter] == 'i'
+		    || format[iter] == 'f'
+		    || format[iter] == 's'))
+			printf(", ");
+		switch (format[iter])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			word = va_arg(args, char *);
+			if (word == NULL)
+				printf("nil");
+			printf("%s", word);
+			break;
+		}
+		iter++;
+	}
+	printf("\n");
+	va_end(args);
+}
