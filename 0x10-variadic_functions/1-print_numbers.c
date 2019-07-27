b5279c483588051487863b94e21610715c01843e
Mitchell <mmoscovics@yahoo.com>
2019-07-27 17:39:37 +0000

    create files for task 1 create 1.c file update header for all tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - tested function, prints all arguments
+ *@separator: separating arguments
+ *@n: number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argpassed;
+	unsigned int iter;
+	int place;
+
+	va_start(argpassed, n);
+	for (iter = 0; iter < n; iter++)
+	{
+		place = va_arg(argpassed, int);
+		printf("%d", place);
+		va_end(argpassed);
+		if (iter < (n - 1))
+		{
+			if (separator == NULL)
+				break;
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
