620c2fae1b4aa713717b691ff16298ad3b0b9bba
Mitchell <mmoscovics@yahoo.com>
2019-08-12 14:03:08 +0000

    [master]create file for task 3 create 3.c program file update header file

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * set_bit - tested function, sets the value of a bit to 1 at index
+ *@n: pointer to given number
+ *@index: given index
+ * Return: 1 if function worked
+ * -1 if error occurs
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask = 1 << index;
+	while (index < 64)
+	{
+		*n |= mask;
+		return (1);
+	}
+	return (-1);
+}
