3ca84bd9bc0316630e13b66c7af16588e7e313bf
Mitchell <mmoscovics@yahoo.com>
2019-08-14 18:35:11 +0000

    [master]update program file for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	for (counter = 0; text_content[counter] != '\0'; counter++)
 		;
