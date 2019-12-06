a8dd1dd1e7439c911d504515058e4fff19cd8282
Mitchell <mmoscovics@yahoo.com>
2019-12-06 16:50:53 +0000

    [Update] betty complience

--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -11,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 	size = (binary_tree_size(tree->left) + 1 +
 		binary_tree_size(tree->right));
 	return (size);
