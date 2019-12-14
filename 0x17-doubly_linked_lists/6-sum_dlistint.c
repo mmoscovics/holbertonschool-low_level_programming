501f0fadd7472c84e61cedcb3989f917c24cdf52
mmoscovics <mmoscovics@yahoo.com>
2019-12-14 19:05:35 +0000

    [Create] task 6 sum of data prototype in header

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns sum of all data of a doubly linked list
+ * @head: pointer to start of linked list
+ * Return: sum of all data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int total;
+	dlistint_t *pos = head;
+
+	for (; pos; pos = pos->next)
+		total += pos->n;
+	return (total);
+}
