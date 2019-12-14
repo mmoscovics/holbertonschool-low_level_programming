3d8230a069d161dfca7245e15fd5997f8aebc629
mmoscovics <mmoscovics@yahoo.com>
2019-12-14 19:37:13 +0000

    [Update] add end conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *pos = *h;
 
 	if (idx == 0)
+	{
 		new = add_dnodeint(h, n);
+		return (new);
+	}
 	for (i = 0; pos; i++, pos = pos->next)
+	{
+		if (pos->next == null && i == idx - 1)
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
 		if (i == idx - 1)
 		{
 			new = malloc(sizeof(dlistint_t));
@@ -28,5 +37,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			pos->next = new;
 			return (new);
 		}
+	}
 	return (NULL);
 }
