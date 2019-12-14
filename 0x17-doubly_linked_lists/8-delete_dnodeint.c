af1f558459d5bab801f69163689b3ab8a7d16fec
mmoscovics <mmoscovics@yahoo.com>
2019-12-14 20:25:12 +0000

    [Create] task 8 delete node at position prototype in header

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at index
+ * @head: pointer to start of linked list
+ * @index: index position
+ * Return: 1 if successful, -1 if fail
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *pos = *head, *hold;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		hold = pos;
+		*head = pos->next;
+		if (pos->next)
+			pos->next->prev = NULL;
+		free(hold);
+		return (1);
+	}
+	for (i = 0; pos; i++, pos = pos->next)
+	{
+		if (pos->next == NULL && i == index - 1)
+		{
+			hold = pos->next;
+			pos->next = NULL;
+			free(hold);
+			return (1);
+		}
+		if (i == index - 1)
+		{
+			hold = pos->next;
+			hold->next->prev = pos;
+			pos->next = hold->next;
+			free(hold);
+			return (1);
+		}
+	}
+	return (-1);
+}
