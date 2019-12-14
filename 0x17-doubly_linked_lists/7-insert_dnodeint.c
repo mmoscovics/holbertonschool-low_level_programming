61630b5026003f5e64e0dfb51c7494b263701927
mmoscovics <mmoscovics@yahoo.com>
2019-12-14 19:30:27 +0000

    [Update] add idx conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *new, *pos = *h;
 
+	if (idx == 0)
+		new = add_dnodeint(h, n);
 	for (i = 0; pos; i++, pos = pos->next)
 		if (i == idx)
 		{
