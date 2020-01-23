050edfe40d5dfc3ba3fec09404d26acf10f32af9
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 16:12:56 +0000

    [Update] malloc correct size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_table_t) * size);
+	table->array = malloc(sizeof(hash_table_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
