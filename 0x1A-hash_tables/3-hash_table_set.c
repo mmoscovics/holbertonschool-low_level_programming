c3b3219a959ca989744e8f38ecca0e1e9e782ba7
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 17:01:39 +0000

    [Update] change index function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || (strlen(key) == 0) || value == NULL)
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
+	index = hash_djb2((const unsigned char *)key) % ht->size);
 	new = ht->array[index];
 	while (new)
 	{
