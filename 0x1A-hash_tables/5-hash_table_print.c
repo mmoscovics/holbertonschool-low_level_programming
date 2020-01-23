0b59cbe138810fd5c0304f79799d0bc304b2bdb3
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 17:54:16 +0000

    [Create] task 5 print hash table, prototype in header

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ *
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int pos;
+	hash_node_t *node = NULL;
+	int node_count = 0;
+
+	if (ht)
+	{
+		printf("{");
+		for (pos = 0; pos < ht->size; pos++)
+		{
+			node = (ht->array)[pos];
+			while (node)
+			{
+				if (node_count != 0)
+					printf(", ");
+				node_count++;
+				printf("'%s': ", node->key);
+				printf("'%s'", node->value);
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
