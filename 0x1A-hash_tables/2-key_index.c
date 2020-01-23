1f0cf723881739b10527787e7d826a2cb6ce3751
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 16:15:55 +0000

    [Update] betty compliance, able to compile

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,14 +2,14 @@
 
 /**
  * key_index - gives you the index of a key
- * 
+ *
  * @key: key
  * @size: size of the array of the hash table
- * 
+ *
  * Return: the index at which the key/value pair should be stored
- * /
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        return(hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
