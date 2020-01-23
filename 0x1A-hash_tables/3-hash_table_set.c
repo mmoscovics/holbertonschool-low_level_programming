0350d7c3f3fba1fa51a47fad7c70a60347dc04d6
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 16:56:46 +0000

    [Update] free value add key len condition

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,14 +15,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new = NULL;
 
-	if (ht == NULL || (key == NULL || value == NULL))
-		return 0;
+	if (ht == NULL || key == NULL || (strlen(key) == 0) || value == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new = ht->array[index];
 	while (new)
 	{
 		if (strcmp(new->key, key) == 0)
 		{
+			free(new->value);
 			new->value = strdup(value);
 			return (1);
 		}
@@ -30,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
-		return 0;
+		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = ht->array[index];
