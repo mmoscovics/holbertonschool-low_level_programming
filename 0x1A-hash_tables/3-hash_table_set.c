98c710ffe41e99d4187fab96e5dd87f840ec707a
mmoscovics <mmoscovics@yahoo.com>
2020-01-23 16:41:32 +0000

    [Create] task 3 add to a hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Function that adds an element to the hash table
+ *
+ * @ht: hash table
+ * @key: key
+ * @value: value to associate with key
+ *
+ * Return: 1 if success, otherwise 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL;
+
+	if (ht == NULL || (key == NULL || value == NULL))
+		return 0;
+	index = key_index((const unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return 0;
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
